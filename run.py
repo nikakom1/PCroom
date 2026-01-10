eacfee67f7d94d73f32ba080162ff1b8d6760794
nikakom1 <komladzenikolozi@gmail.com>
2026-01-10 15:02:24 +0400

    Update run.py

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 from ext import app
-from routes import *
+from routes import home, detailed, cart, login, add_to_cart, edit_product, delete, get_cart_items, remove_from_cart, contact, register, edit_product, logout
 
 app.run (host='0.0.0.0')
+
